6a4232b6c34a55e535acd6ae4ec8472b48941151
Henry Shum <henry@metaseit.com>
2017-01-05 16:19:57 +0800

    minor change in suggested redbean config

--- app/config/rb_config.php
+++ app/config/rb_config.php
@@ -24,6 +24,5 @@ R::setup('sqlite:'.dirname(dirname(dirname(__FILE__))).'/data/sqlite.db');
  *  Freeze RedBean in production environment
  *  ===> Set to TRUE it if you don't want RedBean to CREATE/ALERT table automatically
  **/
-//R::freeze( !in_array(strtolower($_SERVER['SERVER_NAME']), array('127.0.0.1','localhost')) );
 R::freeze(false);
 R::debug(false);
