019e7b60cd9e9bbf29d80fdb4713304f1412b517
Henry Shum <henry.shum@cityu.edu.hk>
2019-09-10 17:41:39 +0800

    modify init script of ajax-uploader & html-editor & datetime-picker because jquery 3.x have no $(document).on('ready')

--- js/fuseboxy.scaffold.js
+++ js/fuseboxy.scaffold.js
@@ -33,175 +33,38 @@ $(function(){
 	}
 
 
-	// init simple-ajax-uploader when document/row/modal show
-	$(document).on('ready ajaxLoad.bsx shown.bs.modal', function(evt){
-		window.setTimeout(function(){
-			$('.scaffold-input-file').not('.simple-ajax-uploader-ready').each(function(){
-				var elementID = $(this).attr('id');
-				// apply ajax-upload to this single field
-				$('#'+elementID).each(function(){
-					// elements
-					var $fieldWrap = $(this);
-					var $field = $fieldWrap.find('input[type=text]');
-					var $uploadBtn = $fieldWrap.find('.btn-upload');
-					var $removeBtn = $fieldWrap.find('.btn-remove');
-					var $undoBtn = $fieldWrap.find('.btn-undo');
-					var $progressWrap = $fieldWrap.find('.progress-wrap');
-					var $preview = $fieldWrap.find('.thumbnail');
-					var $alert = $fieldWrap.find('.alert');
-					// click button to clear selected image
-					$removeBtn.on('click', function(evt){
-						evt.preventDefault();
-						$field.val('');
-						$preview.html('').hide();
-						$undoBtn.show();
-						$removeBtn.hide();
-					}).prop('disabled', false);
-					// click button to restore to original image
-					$undoBtn.on('click', function(evt){
-						evt.preventDefault();
-						$field.val( $undoBtn.attr('data-original-image') );
-						$preview.show().html('<a href="'+$undoBtn.attr('data-original-image')+'" target="_blank"><img src="'+$undoBtn.attr('data-original-image')+'" alt="" /></a>');
-						$undoBtn.hide();
-						$removeBtn.show();
-					}).prop('disabled', false);
-					// validation
-					if ( !$fieldWrap.attr('data-upload-url') ) {
-						alert('attribute [data-upload-url] is required for file upload');
-						$uploadBtn.prop('disabled', true);
-						return false;
-					// add behavior to upload button
-					// ===> it will enable the upload button automatically
-					} else {
-						// param from controller
-						var _uploadUrl = $fieldWrap.attr('data-upload-url');
-						var _progressUrl = $fieldWrap.is('[data-progress-url]') ? $fieldWrap.attr('data-progress-url') : false;
-						var _maxSize = $fieldWrap.is('[data-file-size]') ? (parseFloat($fieldWrap.attr('data-file-size-numeric'))/1024) : false;
-						var _allowedExtensions = $fieldWrap.is('[data-file-type]') ? $fieldWrap.attr('data-file-type').split(',') : false;
-						// init ajax uploader
-						var uploader = new ss.SimpleUpload({
-							//----- essential config -----
-							button: $uploadBtn.prop('disabled', false),
-							url: _uploadUrl,
-							name: $fieldWrap.attr('id'),
-							//----- optional config -----
-							progressUrl: _progressUrl,
-							multiple: false,
-							maxUploads: 1,
-							debug: true,
-							// number of KB (false for default)
-							// ===> javascript use KB for validation
-							// ===> server-side use byte for validation
-							maxSize: _maxSize,
-							// server-upload will block file upload other than below items
-							allowedExtensions: _allowedExtensions,
-							// control what file to show when choosing files
-							//accept: 'image/*',
-							hoverClass: 'btn-hover',
-							focusClass: 'active',
-							disabledClass: 'disabled',
-							responseType: 'json',
-							// validate allowed extension
-							onExtError: function(filename, extension) {
-								$alert.show().html(filename + ' is not a permitted file type.'+"\n\n"+'Only '+$fieldWrap.attr('data-file-type').toUpperCase()+' are allowed.');
-							},
-							// validate file size
-							onSizeError: function(filename, fileSize) {
-								$alert.show().html(filename + ' is too big. ('+$fieldWrap.attr('data-file-size')+' max file size)');
-							},
-							// show progress bar
-							onSubmit: function(filename, ext, btn) {
-								$alert.hide().html('');
-								$preview.hide().html('');
-								if ( $fieldWrap.attr('data-progress-url') ) {
-									$progressWrap.append('<div class="progress progress-striped active"><div class="progress-bar" style="width: 0%;"></div></div>');
-									this.setProgressBar( $progressWrap.find('.progress-bar') );
-									this.setProgressContainer( $progressWrap.find('.progress') );
-									$progressWrap.closest('.row').show();
-								}
-								// browser bug : don't know why must log 'btn' to show progress-bar
-								//console.log(btn);
-							},
-							// start upload
-							startXHR: function() {
-								// Dynamically add a "Cancel" button to be displayed when upload begins
-								// By doing it here ensures that it will only be added in browsers which 
-								// support cancelling uploads
-								var $cancelBtn = $('<span class="btn-cancel-upload"><button class="btn btn-xs btn-block btn-info">Cancel</button></span>');
-								$fieldWrap.find('.progress-abort').append( $cancelBtn );
-								// Adds click event listener that will cancel the upload
-								// The second argument is whether the button should be removed after the upload
-								// true = yes, remove abort button after upload
-								// false/default = do not remove
-								this.setAbortBtn($cancelBtn, true);
-							},
-							// show upload preview (and show remove button)
-							// ===> hide alert, hide progress bar
-							onComplete: function(filename, responseText) {
-								var response = $.parseJSON(responseText);
-								if ( response.success ) {
-									$field.val(response.fileUrl);
-									$preview.show().html('<a href="'+response.fileUrl+'" target="_blank"><img src="'+response.fileUrl+'" alt="" /></a>');
-									if ( $undoBtn.length ) {
-										$undoBtn.show();
-										$removeBtn.hide();
-									} else {
-										$removeBtn.show();
-									}
-									$progressWrap.closest('.row').hide();
-								} else {
-									$alert.html( response.msg ? response.msg : responseText ).show();
-								}
-							}
-						}); // new-simple-upload
-					} // if-data-upload-url
-					// mark flag
-					$(this).addClass('simple-ajax-uploader-ready');
-				}); // each-element-id
-			}); // each-scaffold-input-file
-		}, 1000); // window-setTimeout
-	}); // document-on
-
-
-	// init summernote when document load
-	$('.scaffold-input-wysiwyg').each(function(){
-		$(this).summernote({ 'height' : $(this).height() });
-	}).addClass('summernote-ready');
-	// init summernote when row/modal show
+	// init ajax-uploader when page ready
+	// ===> also when inline-edit & modal shows up
+	fuseboxyScaffold__initAjaxUploader();
 	$(document).on('ajaxLoad.bsx shown.bs.modal', function(evt){
 		window.setTimeout(function(){
-			$('.scaffold-input-wysiwyg:not(.summernote-ready)').each(function(){
-				$(this).summernote({ 'height' : $(this).height() });
-			}).addClass('summernote-ready');
+			fuseboxyScaffold__initAjaxUploader();
+		}, 1000);
+	});
+
+
+	// init html-editor when page ready
+	// ===> also when inline-edit & modal shows up
+	fuseboxyScaffold__initHtmlEditor();
+	$(document).on('ajaxLoad.bsx shown.bs.modal', function(evt){
+		window.setTimeout(function(){
+			fuseboxyScaffold__initHtmlEditor();
 		}, 500);
 	});
 
 
-	// init datetime picker when document/row/modal show
+	// init datetime-picker when page ready
+	// ===> also when inline-edit & modal shows up
+	fuseboxyScaffold__initDatetimePicker();
 	$(document).on('ready ajaxLoad.bsx shown.bs.modal', function(evt){
 		window.setTimeout(function(){
-			$('.scaffold-input-date,.scaffold-input-time,.scaffold-input-datetime').not('.datetimepicker-ready').each(function(){
-				if ( $(this).hasClass('scaffold-input-date') ) {
-					var format = 'YYYY-MM-DD';
-				} else if ( $(this).hasClass('scaffold-input-time') ) {
-					var format = 'HH:mm';
-				} else {
-					var format = 'YYYY-MM-DD HH:mm';
-				}
-				// transform
-				$(this).datetimepicker({
-					'format' : format,
-					'sideBySide' : true
-				});
-				// mark flag
-				$(this).addClass('datetimepicker-ready');
-			});
+			fuseboxyScaffold__initDatetimePicker();
 		}, 500);
 	});
 
 
 	// sticky header
-	$(window).scroll(function(){
+	$(window).on('scroll', function(evt){
 		$('.scaffold-header').each(function(){
 			var header = this;
 			var windowScrollTop = $(window).scrollTop();
@@ -239,8 +102,166 @@ $(function(){
 					// clear retained state
 					.removeData('original-state')
 			}
+		}); // scaffold-header-each
+	}); // window-scroll
+
+
+}); // document-ready
+
+
+function fuseboxyScaffold__initDatetimePicker(){
+	$('.scaffold-input-date,.scaffold-input-time,.scaffold-input-datetime').not('.datetimepicker-ready').each(function(){
+		// config
+		if ( $(this).hasClass('scaffold-input-date') ) {
+			var format = 'YYYY-MM-DD';
+		} else if ( $(this).hasClass('scaffold-input-time') ) {
+			var format = 'HH:mm';
+		} else {
+			var format = 'YYYY-MM-DD HH:mm';
+		}
+		// transform
+		$(this).datetimepicker({
+			'format' : format,
+			'sideBySide' : true
 		});
+		// mark complete
+		$(this).addClass('datetimepicker-ready');
 	});
+}
 
 
-});
+function fuseboxyScaffold__initHtmlEditor(){
+	$('.scaffold-input-wysiwyg:not(.summernote-ready').each(function(){
+		// transform
+		$(this).summernote({ 'height' : $(this).height() });
+		// mark complete
+		$(this).addClass('summernote-ready');
+	});
+}
+
+
+function fuseboxyScaffold__initAjaxUploader(){
+	$('.scaffold-input-file:not(.simple-ajax-uploader-ready)').each(function(){
+		var elementID = $(this).attr('id');
+		// apply ajax-upload to this single field
+		$('#'+elementID).each(function(){
+			// elements
+			var $fieldWrap = $(this);
+			var $field = $fieldWrap.find('input[type=text]');
+			var $uploadBtn = $fieldWrap.find('.btn-upload');
+			var $removeBtn = $fieldWrap.find('.btn-remove');
+			var $undoBtn = $fieldWrap.find('.btn-undo');
+			var $progressWrap = $fieldWrap.find('.progress-wrap');
+			var $preview = $fieldWrap.find('.thumbnail');
+			var $alert = $fieldWrap.find('.alert');
+			// click button to clear selected image
+			$removeBtn.on('click', function(evt){
+				evt.preventDefault();
+				$field.val('');
+				$preview.html('').hide();
+				$undoBtn.show();
+				$removeBtn.hide();
+			}).prop('disabled', false);
+			// click button to restore to original image
+			$undoBtn.on('click', function(evt){
+				evt.preventDefault();
+				$field.val( $undoBtn.attr('data-original-image') );
+				$preview.show().html('<a href="'+$undoBtn.attr('data-original-image')+'" target="_blank"><img src="'+$undoBtn.attr('data-original-image')+'" alt="" /></a>');
+				$undoBtn.hide();
+				$removeBtn.show();
+			}).prop('disabled', false);
+			// validation
+			if ( !$fieldWrap.attr('data-upload-url') ) {
+				alert('attribute [data-upload-url] is required for file upload');
+				$uploadBtn.prop('disabled', true);
+				return false;
+			// add behavior to upload button
+			// ===> it will enable the upload button automatically
+			} else {
+				// param from controller
+				var _uploadUrl = $fieldWrap.attr('data-upload-url');
+				var _progressUrl = $fieldWrap.is('[data-progress-url]') ? $fieldWrap.attr('data-progress-url') : false;
+				var _maxSize = $fieldWrap.is('[data-file-size]') ? (parseFloat($fieldWrap.attr('data-file-size-numeric'))/1024) : false;
+				var _allowedExtensions = $fieldWrap.is('[data-file-type]') ? $fieldWrap.attr('data-file-type').split(',') : false;
+				// init ajax uploader
+				var uploader = new ss.SimpleUpload({
+					//----- essential config -----
+					button: $uploadBtn.prop('disabled', false),
+					url: _uploadUrl,
+					name: $fieldWrap.attr('id'),
+					//----- optional config -----
+					progressUrl: _progressUrl,
+					multiple: false,
+					maxUploads: 1,
+					debug: true,
+					// number of KB (false for default)
+					// ===> javascript use KB for validation
+					// ===> server-side use byte for validation
+					maxSize: _maxSize,
+					// server-upload will block file upload other than below items
+					allowedExtensions: _allowedExtensions,
+					// control what file to show when choosing files
+					//accept: 'image/*',
+					hoverClass: 'btn-hover',
+					focusClass: 'active',
+					disabledClass: 'disabled',
+					responseType: 'json',
+					// validate allowed extension
+					onExtError: function(filename, extension) {
+						$alert.show().html(filename + ' is not a permitted file type.'+"\n\n"+'Only '+$fieldWrap.attr('data-file-type').toUpperCase()+' are allowed.');
+					},
+					// validate file size
+					onSizeError: function(filename, fileSize) {
+						$alert.show().html(filename + ' is too big. ('+$fieldWrap.attr('data-file-size')+' max file size)');
+					},
+					// show progress bar
+					onSubmit: function(filename, ext, btn) {
+						$alert.hide().html('');
+						$preview.hide().html('');
+						if ( $fieldWrap.attr('data-progress-url') ) {
+							$progressWrap.append('<div class="progress progress-striped active"><div class="progress-bar" style="width: 0%;"></div></div>');
+							this.setProgressBar( $progressWrap.find('.progress-bar') );
+							this.setProgressContainer( $progressWrap.find('.progress') );
+							$progressWrap.closest('.row').show();
+						}
+						// browser bug : don't know why must log 'btn' to show progress-bar
+						//console.log(btn);
+					},
+					// start upload
+					startXHR: function() {
+						// Dynamically add a "Cancel" button to be displayed when upload begins
+						// By doing it here ensures that it will only be added in browsers which 
+						// support cancelling uploads
+						var $cancelBtn = $('<span class="btn-cancel-upload"><button class="btn btn-xs btn-block btn-info">Cancel</button></span>');
+						$fieldWrap.find('.progress-abort').append( $cancelBtn );
+						// Adds click event listener that will cancel the upload
+						// The second argument is whether the button should be removed after the upload
+						// true = yes, remove abort button after upload
+						// false/default = do not remove
+						this.setAbortBtn($cancelBtn, true);
+					},
+					// show upload preview (and show remove button)
+					// ===> hide alert, hide progress bar
+					onComplete: function(filename, responseText) {
+						var response = $.parseJSON(responseText);
+						if ( response.success ) {
+							$field.val(response.fileUrl);
+							$preview.show().html('<a href="'+response.fileUrl+'" target="_blank"><img src="'+response.fileUrl+'" alt="" /></a>');
+							if ( $undoBtn.length ) {
+								$undoBtn.show();
+								$removeBtn.hide();
+							} else {
+								$removeBtn.show();
+							}
+							$progressWrap.closest('.row').hide();
+						} else {
+							$alert.html( response.msg ? response.msg : responseText ).show();
+						}
+					}
+				}); // new-simple-upload
+			} // if-data-upload-url
+			// mark complete
+			$(this).addClass('simple-ajax-uploader-ready');
+		}); // each-element-id
+	}); // each-scaffold-input-file
+} // function
