ba91c51d3cd3aaeda66f19e31721108bf7d44349
Henry Shum <henry.shum@cityu.edu.hk>
2019-10-03 18:30:15 +0800

    apply dropdown menu to new/quick button to create multiple new items by one click

--- app/view/scaffold/header.button.php
+++ app/view/scaffold/header.button.php
@@ -1,26 +1,77 @@
+<?php /*
+<fusedoc>
+	<io>
+		<in>
+			<structure name="$xfa">
+				<string name="quick" optional="yes" />
+				<string name="new" optional="yes" />
+			</structure>
+		</in>
+		<out />
+	</io>
+</fusedoc>
+*/ ?>
 <?php if ( isset($xfa['quick']) ) : ?>
-	<a
-		href="<?php echo F::url($xfa['quick']); ?>"
-		class="btn btn-sm py-0 px-1 btn-light scaffold-btn-quick"
-		data-toggle="ajax-load"
-		data-toggle-loading="none"
-		data-toggle-mode="after"
-		data-target="<?php echo "#{$scaffold['beanType']}-header"; ?>"
-	><i class="fa fa-sort-amount-down"></i> Quick</a>
-<?php endif; ?>
-<?php if ( isset($xfa['new']) ) : ?>
-	<a
-		href="<?php echo F::url($xfa['new']); ?>"
-		class="btn btn-sm py-0 px-1 btn-info scaffold-btn-new"
-		<?php if ( $scaffold['editMode'] == 'modal' ) : ?>
-			data-toggle="modal"
-			data-target="<?php echo "#{$scaffold['beanType']}-modal"; ?>"
-			data-toggle-loading="none"
-		<?php elseif ( $scaffold['editMode'] == 'inline' ) : ?>
+	<div class="btn-group">
+		<!-- main button -->
+		<a
+			href="<?php echo F::url($xfa['quick']); ?>"
+			class="btn btn-sm py-0 px-1 btn-light scaffold-btn-quick"
 			data-toggle="ajax-load"
+			data-toggle-loading="none"
 			data-toggle-mode="after"
 			data-target="<?php echo "#{$scaffold['beanType']}-header"; ?>"
-			data-toggle-loading="none"
+		><i class="fa fa-sort-amount-down"></i> Quick</a>
+		<!-- dropdown button -->
+		<button 
+			type="button" 
+			class="btn btn-sm py-0 px-1 btn-light dropdown-toggle dropdown-toggle-split scaffold-btn-quick-multiple" 
+			data-toggle="dropdown" 
+			aria-haspopup="true" 
+			aria-expanded="false" 
+		></button>
+		<!-- dropdown -->
+		<ul class="dropdown-menu dropdown-menu-right" style="min-width: 100%;">
+			<?php for ( $i=2; $i<=9; $i++ ) : ?>
+				<li class="dropdown-item text-center small"><?php echo $i; ?></li>
+			<?php endfor; ?>
+		</ul>
+	</div>
+<?php endif; ?>
+
+
+<?php if ( isset($xfa['new']) ) : ?>
+	<div class="btn-group">
+		<!-- main button -->
+		<a
+			href="<?php echo F::url($xfa['new']); ?>"
+			class="btn btn-sm py-0 px-1 btn-info scaffold-btn-new"
+			<?php if ( $scaffold['editMode'] == 'modal' ) : ?>
+				data-toggle="modal"
+				data-target="<?php echo "#{$scaffold['beanType']}-modal"; ?>"
+				data-toggle-loading="none"
+			<?php elseif ( $scaffold['editMode'] == 'inline' ) : ?>
+				data-toggle="ajax-load"
+				data-toggle-mode="after"
+				data-target="<?php echo "#{$scaffold['beanType']}-header"; ?>"
+				data-toggle-loading="none"
+			<?php endif; ?>
+		><i class="fa fa-plus"></i> New</a>
+		<?php if ( !isset($xfa['quick']) ) : ?>
+			<!-- dropdown button -->
+			<button 
+				type="button" 
+				class="btn btn-sm py-0 px-1 btn-info dropdown-toggle dropdown-toggle-split scaffold-btn-new-multiple" 
+				data-toggle="dropdown" 
+				aria-haspopup="true" 
+				aria-expanded="false" 
+			></button>
+			<!-- dropdown -->
+			<ul class="dropdown-menu dropdown-menu-right" style="min-width: 100%;">
+				<?php for ( $i=2; $i<=9; $i++ ) : ?>
+					<li class="dropdown-item text-center small"><?php echo $i; ?></li>
+				<?php endfor; ?>
+			</ul>
 		<?php endif; ?>
-	><i class="fa fa-plus"></i> New</a>
+	</div>
 <?php endif; ?>
