a662cad2cad1da5befbebc7d8ff1eda57d218606
henrygotmojo <henrygotmojo@gmail.com>
2019-11-13 12:57:13 +0800

    further trim white-space of scaffold-row and do not wrap button by div

--- app/view/scaffold/row.php
+++ app/view/scaffold/row.php
@@ -24,112 +24,103 @@
 <div id="<?php echo $scaffold['beanType']; ?>-row-<?php echo $bean->id; ?>" class="<?php echo $scaffold['beanType']; ?>-row scaffold-row small">
 	<table class="table table-hover table-sm mb-0">
 		<tbody>
-			<tr <?php if ( !empty($bean->disabled) ) : ?>style="background: #ddd; opacity: .5;"<?php endif; ?>>
-				<?php foreach ( $scaffold['listField'] as $key => $val ) : ?>
-					<?php $cols = array_map('trim', explode('|', is_numeric($key) ? $val : $key)); ?>
-					<?php $colWidth = is_numeric($key) ? '' : $val; ?>
-					<td class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>;">
-						<?php foreach ( $cols as $i => $col ) : ?>
-							<?php
-								$objectName   = ( substr($col, -3) == '_id' ) ? str_replace('_id', '', $col) : $col;
-								$isManyToMany = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'many-to-many' );
-								$isOneToMany  = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'one-to-many' );
-								$isCheckbox   = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'checkbox' );
-								$isObject     = is_object($bean[$objectName]);
-								$isFile       = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'file' );
-								$isHidden     = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'hidden' );
-								$isWYSIWYG    = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'wysiwyg' );
-								$isURL        = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'url' );
-								$isHR         = ( strlen($col) and !strlen(str_replace('-', '', $col)) );
-							?>
-							<div class="col-<?php echo $col; ?> <?php if ( $i != 0 ) echo 'small text-muted'; ?> <?php if ( $isHidden ) echo 'hidden'; ?>">
-								<!-- preview : show thumbnail -->
-								<?php if ( !empty($bean[$col]) and !empty($scaffold['fieldConfig'][$col]['preview']) ) : ?>
-									<div>
-										<a
-											title="<?php echo basename($bean[$col]); ?>"
-											href="<?php echo $bean[$col]; ?>"
-											target="_blank"
-											data-fancybox
-										><img
-											alt="<?php echo basename($bean[$col]); ?>"
-											src="<?php echo $bean[$col]; ?>"
-											class="img-thumbnail mb-0 mt-1"
-											style="max-width: 100%; <?php if ( isset($bean->disabled) and $bean->disabled ) echo 'opacity: .5;'; ?> <?php if ( !empty($scaffold['fieldConfig'][$col]['style']) ) echo $scaffold['fieldConfig'][$col]['style']; ?>"
-										/></a>
-									</div>
-								<!-- file : show link -->
-								<?php elseif ( $isFile and !empty($bean[$col]) ) : ?>
-									<a
+			<tr <?php if ( !empty($bean->disabled) ) : ?>class="table-active; op-50;"<?php endif; ?>><?php
+				// go through each item in scaffold-listField config
+				foreach ( $scaffold['listField'] as $key => $val ) :
+					$cols = array_map('trim', explode('|', is_numeric($key) ? $val : $key));
+					$colWidth = is_numeric($key) ? '' : $val;
+					// display : field
+					?><td class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>;"><?php
+						// go through each field
+						foreach ( $cols as $i => $col ) :
+							// determine field format
+							$objectName   = ( substr($col, -3) == '_id' ) ? str_replace('_id', '', $col) : $col;
+							$isManyToMany = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'many-to-many' );
+							$isOneToMany  = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'one-to-many' );
+							$isCheckbox   = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'checkbox' );
+							$isObject     = is_object($bean[$objectName]);
+							$isFile       = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'file' );
+							$isHidden     = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'hidden' );
+							$isWYSIWYG    = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'wysiwyg' );
+							$isURL        = ( isset($scaffold['fieldConfig'][$col]['format']) and $scaffold['fieldConfig'][$col]['format'] == 'url' );
+							$isHR         = ( strlen($col) and !strlen(str_replace('-', '', $col)) );
+							// display : each field
+							?><div class="col-<?php echo $col; ?> <?php if ( $i != 0 ) echo 'small text-muted'; ?> <?php if ( $isHidden ) echo 'hidden'; ?>"><?php
+								// preview : show thumbnail
+								if ( !empty($bean[$col]) and !empty($scaffold['fieldConfig'][$col]['preview']) ) :
+									?><div><a
+										title="<?php echo basename($bean[$col]); ?>"
+										href="<?php echo $bean[$col]; ?>"
+										target="_blank"
+										data-fancybox
+									><img
+										alt="<?php echo basename($bean[$col]); ?>"
+										src="<?php echo $bean[$col]; ?>"
+										class="img-thumbnail mb-0 mt-1"
+										style="max-width: 100%; <?php if ( isset($bean->disabled) and $bean->disabled ) echo 'opacity: .5;'; ?> <?php if ( !empty($scaffold['fieldConfig'][$col]['style']) ) echo $scaffold['fieldConfig'][$col]['style']; ?>"
+									/></a></div><?php
+								// file : show link
+								elseif ( $isFile and !empty($bean[$col]) ) :
+									?><a
 										href="<?php echo $bean[$col]; ?>"
 										style="word-break: break-all;"
 										target="_blank"
-									><?php echo basename($bean[$col]); ?></a>
-								<!-- checkbox : turn list into items -->
-								<?php elseif ( $isCheckbox and !empty($bean[$col]) ) : ?>
-									<div><?php echo str_replace('|', '</div><div>', $bean[$col]); ?></div>
-								<!-- many-to-many : show alias/name/etc. -->
-								<?php elseif ( $isManyToMany ) : ?>
-									<?php foreach ( $bean['shared'.ucfirst($objectName)] as $associateBean ) : ?>
-										<div><?php
-											if ( isset($associateBean->alias) ) {
-												echo $associateBean->alias;
-											} elseif ( isset($associateBean->name) ) {
-												echo $associateBean->name;
-											} else {
-												echo $associateBean->id;
-											}
-										?></div>
-									<?php endforeach; ?>
-								<!-- one-to-many : show alias/name/etc. -->
-								<?php elseif ( $isOneToMany ) : ?>
-									<?php foreach ( $bean['own'.ucfirst($objectName)] as $associateBean ) : ?>
-										<div><?php
-											if ( isset($associateBean->alias) ) {
-												echo $associateBean->alias;
-											} elseif ( isset($associateBean->name) ) {
-												echo $associateBean->name;
-											} else {
-												echo $associateBean->id;
-											}
-										?></div>
-									<?php endforeach; ?>
-								<!-- object : show alias/name/etc. -->
-								<?php elseif ( $isObject ) : ?>
-									<div><?php
-										if ( isset($bean[$objectName]->alias) ) {
-											echo $bean[$objectName]->alias;
-										} elseif ( isset($bean[$objectName]->name) ) {
-											echo $bean[$objectName]->name;
-										} else {
-											echo $bean[$objectName]->id;
-										}
-									?></div>
-								<!-- url : show link -->
-								<?php elseif ( $isURL ) : ?>
-									<div><a
+									><?php echo basename($bean[$col]); ?></a><?php
+								// checkbox : turn list into items
+								elseif ( $isCheckbox and !empty($bean[$col]) ) :
+									?><div><?php echo str_replace('|', '</div><div>', $bean[$col]); ?></div><?php
+								// many-to-many : show alias/name/etc.
+								elseif ( $isManyToMany ) :
+									foreach ( $bean['shared'.ucfirst($objectName)] as $associateBean ) :
+										?><div><?php
+											if     ( isset($associateBean->alias) ) : echo $associateBean->alias;
+											elseif ( isset($associateBean->name)  ) : echo $associateBean->name;
+											else                                    : echo $associateBean->id;
+											endif;
+										?></div><?php
+									endforeach;
+								// one-to-many : show alias/name/etc.
+								elseif ( $isOneToMany ) :
+									foreach ( $bean['own'.ucfirst($objectName)] as $associateBean ) :
+										?><div><?php
+											if     ( isset($associateBean->alias) ) : echo $associateBean->alias;
+											elseif ( isset($associateBean->name)  ) : echo $associateBean->name;
+											else                                    : echo $associateBean->id;
+											endif;
+										?></div><?php
+									endforeach;
+								// object : show alias/name/etc.
+								elseif ( $isObject ) :
+									?><div><?php
+										if     ( isset($bean[$objectName]->alias) ) : echo $bean[$objectName]->alias;
+										elseif ( isset($bean[$objectName]->name)  ) : echo $bean[$objectName]->name;
+										else                                        : echo $bean[$objectName]->id;
+										endif;
+									?></div><?php
+								// url : show link
+								elseif ( $isURL ) :
+									?><div><a
 										href="<?php echo $bean[$col]; ?>"
 										style="word-break: break-all;"
 										target="_blank"
-									><?php echo $bean[$col]; ?></a></div>
-								<!-- wysiwyg : show html -->
-								<?php elseif ( $isWYSIWYG ) : ?>
-									<div><?php echo $bean[$col]; ?></div>
-								<!-- horizontal line -->
-								<?php elseif ( $isHR ) : ?>
-									<div><hr style="margin: .75em 0;" /></div>
-								<!-- show text -->
-								<?php else : ?>
-									<div><?php echo nl2br($bean[$col]); ?></div>
-								<?php endif; ?>
-							</div>
-						<?php endforeach; ?>
-					</td>
-				<?php endforeach; ?>
-				<td class="col-button text-nowrap">
-					<div class="text-right"><?php include 'row.button.php'; ?></div>
-				</td>
-			</tr>
+									><?php echo $bean[$col]; ?></a></div><?php
+								// wysiwyg : show html
+								elseif ( $isWYSIWYG ) :
+									?><div><?php echo $bean[$col]; ?></div><?php
+								// horizontal line
+								elseif ( $isHR ) :
+									?><div><hr class="my-2 mx-0" /></div><?php
+								// show text
+								else :
+									?><div><?php echo nl2br($bean[$col]); ?></div><?php
+								endif;
+							?></div><!--/.col-XXX--><?php
+						endforeach; // foreach-cols
+					?></td><!--/.col-XXX-YYY--><?php
+				endforeach; // foreach-scaffold-listField
+				// display : button
+				?><td class="col-button text-nowrap text-right"><?php include 'row.button.php'; ?></td><?php
+			?></tr>
 		</tbody>
 	</table>
-</div>
+</div><!--/.scaffold-row-->
