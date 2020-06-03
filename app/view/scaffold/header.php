4351c32f28efa90f96237c1e739afd58c7a8c6ff
henrygotmojo <henrygotmojo@gmail.com>
2020-06-03 17:03:06 +0800

    adjust header link color and spacing

--- app/view/scaffold/header.php
+++ app/view/scaffold/header.php
@@ -29,43 +29,50 @@
 <div id="<?php echo $scaffold['beanType']; ?>-header" class="scaffold-header bg-white small">
 	<table class="table table-sm table-borderless border-bottom mb-0">
 		<thead>
-			<tr>
-				<?php foreach ( $scaffold['listField'] as $key => $val ) : ?>
-					<?php $cols = array_map('trim', explode('|', is_numeric($key) ? $val : $key)); ?>
-					<?php $colWidth = is_numeric($key) ? '' : $val; ?>
-					<th scope="col" class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>">
-						<?php foreach ( $cols as $colIndex => $col ) : ?>
-							<?php $isHR = ( strlen($col) and !strlen(str_replace('-', '', $col))); ?>
-							<?php if ( !$isHR ) : ?>
-								<?php
-									$isSortByThisField = ( isset($arguments['sortField']) and $arguments['sortField'] == $col );
-									$isAscendingOrder = ( empty($arguments['sortRule']) or strtolower($arguments['sortRule']) == 'asc' );
-									if ( isset($xfa['sort']) ) {
-										$sortUrl = "{$xfa['sort']}&sortField={$col}";
-										if ( $isSortByThisField and $isAscendingOrder ) $sortUrl .= '&sortRule=desc';
-									}
-									$headerText = $scaffold['fieldConfig'][$col]['label'];
-									if ( $isSortByThisField and !empty($headerText) ) {
-										$headerIcon = $isAscendingOrder ? 'fa fa-caret-up' : 'fa fa-caret-down';
-										$headerText .= " <i class='{$headerIcon}'></i>";
-									}
-									if ( $colIndex > 0 and !empty($headerText) ) {
-										$headerText = "<small class='text-muted'>/ {$headerText}</small>";
-									}
-									$headerText = "<span class='col-{$col} text-nowrap'>{$headerText}</span>";
-								?>
-								<?php if ( isset($xfa['sort']) ) : ?>
-									<a href="<?php echo F::url($sortUrl); ?>" class="scaffold-btn-sort"><?php echo $headerText; ?></a>
-								<?php else : ?>
-									<?php echo $headerText; ?>
-								<?php endif; ?>
-							<?php endif; ?>
-						<?php endforeach; ?>
-					</th>
-				<?php endforeach; ?>
-				<th class="col-button">
-					<div class="text-right"><?php include F::appPath('view/scaffold/header.button.php'); ?></div>
-				</th>
+			<tr><?php
+				// go through list config
+				foreach ( $scaffold['listField'] as $key => $val ) :
+					$cols = array_map('trim', explode('|', is_numeric($key) ? $val : $key));
+					$colWidth = is_numeric($key) ? '' : $val;
+					// header field
+					?><th scope="col" class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>"><?php
+						foreach ( $cols as $colIndex => $col ) :
+							$isHR = ( strlen($col) and !strlen(str_replace('-', '', $col)));
+							if ( !$isHR ) :
+								$isSortByThisField = ( isset($arguments['sortField']) and $arguments['sortField'] == $col );
+								$isAscendingOrder = ( empty($arguments['sortRule']) or strtolower($arguments['sortRule']) == 'asc' );
+								// prepare link (when necessary)
+								if ( isset($xfa['sort']) ) :
+									$sortUrl = "{$xfa['sort']}&sortField={$col}";
+									if ( $isSortByThisField and $isAscendingOrder ) $sortUrl .= '&sortRule=desc';
+								endif;
+								// header text
+								$headerText = $scaffold['fieldConfig'][$col]['label'];
+								// show sort icon (when necessary)
+								if ( $isSortByThisField and !empty($headerText) ) :
+									$headerIcon = $isAscendingOrder ? 'fa fa-caret-up' : 'fa fa-caret-down';
+									$headerText .= "<i class='{$headerIcon} ml-1'></i>";
+								endif;
+								// adjust header size
+								$wrapperClass = array("col-{$col} text-nowrap");
+								$wrapperClass[] = ( $colIndex > 0 and !empty($headerText) ) ? 'small text-muted' : 'text-dark';
+								$headerText = '<span class="'.implode(' ', $wrapperClass).'">'.$headerText.'</span>';
+								// wrap by link (when necessary)
+								if ( isset($xfa['sort']) ) :
+									$headerText = '<a href="'.F::url($sortUrl);.'" class="scaffold-btn-sort">'.$headerText.'</a>';
+								else :
+								// separator
+								if ( $colIndex > 0 ) :
+									?><small class="text-muted mx-1">/</small><?php
+								endif;
+								// display header
+								echo $headerText;
+							endif; // if-not-hr
+						endforeach; // foreach-col
+					?></th><?php
+				endforeach; // foreach-list-field
+				// button
+				?><th class="col-button text-right"><?php include F::appPath('view/scaffold/header.button.php'); ?></th>
 			</tr>
 		</thead>
 	</table>
