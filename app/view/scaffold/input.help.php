a0ae75b3236d5d4c94ca8bb42990ba6ecf3fc55c
henrygotmojo <henrygotmojo@gmail.com>
2020-06-08 13:01:44 +0800

    add fusedoc

--- app/view/scaffold/input.help.php
+++ app/view/scaffold/input.help.php
@@ -1,1 +1,13 @@
+<?php /*
+<fusedoc>
+	<io>
+		<in>
+			<structure name="$field">
+				<string name="help" comments="help text show after input field" />
+			</structure>
+		</in>
+		<out />
+	</io>
+</fusedoc>
+*/ ?>
 <small class="form-text px-1 rounded bg-light text-info"><?php echo $field['help']; ?></small>
--- app/view/scaffold/input.pre_help.php
+++ app/view/scaffold/input.pre_help.php
@@ -1,1 +1,13 @@
+<?php /*
+<fusedoc>
+	<io>
+		<in>
+			<structure name="$field">
+				<string name="pre-help" comments="help text show before input field" />
+			</structure>
+		</in>
+		<out />
+	</io>
+</fusedoc>
+*/ ?>
 <small class="form-text text-muted mb-1"><?php echo $field['pre-help']; ?></small>
