3d466cf50930868ec084166629713542e5410283
henrygotmojo <henrygotmojo@gmail.com>
2020-06-08 13:02:11 +0800

    apply class and style to input-output

--- app/view/scaffold/input.output.php
+++ app/view/scaffold/input.output.php
@@ -1,1 +1,18 @@
-<div class="form-control-plaintext form-control-sm"><?php echo $field['value']; ?></div>
+<?php /*
+<fusedoc>
+	<io>
+		<in>
+			<structure name="$field">
+				<string name="icon" optional="yes" />
+				<string name="value" />
+				<string name="class" optional="yes" />
+				<string name="style" optional="yes" />
+			</structure>
+		</in>
+		<out />
+	</io>
+</fusedoc>
+*/ ?>
+<div class="form-control-plaintext form-control-sm <?php if ( isset($field['class']) ) echo $field['class']; ?>"
+	<?php if ( isset($field['style']) ) : ?>style="<?php echo $field['style']; ?>"<?php endif; ?>
+><?php echo $field['value']; ?></div>
