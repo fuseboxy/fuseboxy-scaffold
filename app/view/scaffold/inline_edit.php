20b3a5bfeaaa9ff7366455b64ea70432e1789fe7
henrygotmojo <henrygotmojo@gmail.com>
2020-10-28 09:41:10 +0800

    add col-xxx class to div wrapper of fields in inline-edit

--- app/view/scaffold/inline_edit.php
+++ app/view/scaffold/inline_edit.php
@@ -25,48 +25,42 @@
 		<out />
 	</io>
 </fusedoc>
-*/ ?>
-<?php $recordID = empty($bean->id) ? uuid() : $bean->id; ?>
-<div
+*/
+$recordID = empty($bean->id) ? uuid() : $bean->id;
+?><form
 	id="<?php echo $scaffold['beanType']; ?>-inline-edit-<?php echo $recordID; ?>"
-	class="<?php echo $scaffold['beanType']; ?>-inline-edit scaffold-inline-edit"
+	class="<?php echo $scaffold['beanType']; ?>-inline-edit scaffold-inline-edit form-horizontal"
+	<?php if ( isset($xfa['submit']) ) : ?>
+		method="post"
+		action="<?php echo F::url($xfa['submit']); ?>"
+		data-toggle="ajax-submit"
+		data-target="#<?php echo $scaffold['beanType']; ?>-inline-edit-<?php echo $recordID; ?>"
+	<?php endif; ?>
 >
-	<form
-		class="form-horizontal"
-		<?php if ( isset($xfa['submit']) ) : ?>
-			method="post"
-			action="<?php echo F::url($xfa['submit']); ?>"
-			data-toggle="ajax-submit"
-			data-target="#<?php echo $scaffold['beanType']; ?>-inline-edit-<?php echo $recordID; ?>"
-		<?php endif; ?>
-	>
-		<table class="table table-hover table-sm mb-0">
-			<tr>
-				<?php foreach ( $scaffold['listField'] as $key => $val ) : ?>
-					<?php $cols = explode('|', is_numeric($key) ? $val : $key); ?>
-					<?php $colWidth = is_numeric($key) ? '' : $val; ?>
-					<td class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>;">
-						<?php foreach ( $cols as $i => $col ) : ?>
-							<div class="form-group mb-1">
-								<div class="w-100"><?php
-									if ( isset($scaffold['fieldConfig'][$col]) ) :
-										$field = $scaffold['fieldConfig'][$col];
-										$field['name'] = $col;
-										include F::appPath('view/scaffold/input.php');
-									else :
-										?><div class="form-control" readonly>
-											<em class="small text-muted text-nowrap">Field [<?php echo $col; ?>] is undefined</em>
-										</div><?php
-									endif;
-								?></div>
-							</div>
-						<?php endforeach; ?>
-					</td>
-				<?php endforeach; ?>
-				<td class="col-button text-nowrap">
-					<div class="text-right"><?php include F::appPath('view/scaffold/inline_edit.button.php'); ?></div>
-				</td>
-			</tr>
-		</table>
-	</form>
-</div>
+	<table class="table table-hover table-sm mb-0">
+		<tr><?php
+			foreach ( $scaffold['listField'] as $key => $val ) :
+				$cols = explode('|', is_numeric($key) ? $val : $key);
+				$colWidth = is_numeric($key) ? '' : $val;
+				?><td class="col-<?php echo implode('-', $cols); ?>" width="<?php echo $colWidth; ?>;"><?php
+					foreach ( $cols as $i => $col ) :
+						?><div class="col-<?php echo $col; ?> form-group mb-1"><?php
+							if ( isset($scaffold['fieldConfig'][$col]) ) :
+								$field = $scaffold['fieldConfig'][$col];
+								$field['name'] = $col;
+								include F::appPath('view/scaffold/input.php');
+							else :
+								?><div class="form-control" readonly>
+									<em class="small text-muted text-nowrap">Field [<?php echo $col; ?>] is undefined</em>
+								</div><?php
+							endif;
+						?></div><?php
+					endforeach;
+				?></td><?php
+			endforeach;
+			?><td class="col-button text-nowrap">
+				<div class="text-right"><?php include F::appPath('view/scaffold/inline_edit.button.php'); ?></div>
+			</td>
+		</tr>
+	</table>
+</form>
